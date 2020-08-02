006ce37c04868e3dda76e0782273ef3db999b85f
TuHuynh1996 <57967909+TuHuynh1996@users.noreply.github.com>
2020-08-02 22:42:40 +0700

    Update ReposetoryJoinTable.java

--- QuerySqlJava/JpaReposetory/ReposetoryJoinTable.java
+++ QuerySqlJava/JpaReposetory/ReposetoryJoinTable.java
@@ -6,10 +6,10 @@ public interface EntityRepositoryLeftJoinTable extends Repository<Entity1, Long>
 public List<ReleaseDateType> FindAllWithDescriptionQuery(@Param("option") String option);
 //In this case a query annotation is not need since spring constructs the query from the method name
 //Entity2_Option is a variable of table join
-public List<ReleaseDateType> findByEntity2_Option(Integer id); 
+public List<ReleaseDateType> findByEntity2_Option(String option); 
 
 // add pagination
 // check file Reposetory in same folder for more info
-public List<ReleaseDateType> findByEntity2_Option(Integer id, Pageable of); 
+public List<ReleaseDateType> findByEntity2_Option(String option, Pageable of); 
 }
 
