8d322b30526cacfd82d2c83b46493a47ac81303a
TuHuynh1996 <tuhuynh962285164@gmail.com>
2021-04-18 23:13:19 +0700

    Update module send mail

---
+++ MavenBase/mail/src/main/java/com/example/mail/MailInformation.java
@@ -0,0 +1,100 @@
+package com.example.mail;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * The Class MailInformation.
+ */
+@Component
+public class MailInformation {
+
+	/** The host. */
+	@Value("${spring.mail.host}")
+	private String host;
+
+	/** The port. */
+	@Value("${spring.mail.port}")
+	private int port;
+
+	/** The username. */
+	@Value("${spring.mail.username}")
+	private String username;
+
+	/** The password. */
+	@Value("${spring.mail.password}")
+	private String password;
+
+	/**
+	 * Gets the host.
+	 *
+	 * @return the host
+	 */
+	public String getHost() {
+		return host;
+	}
+
+	/**
+	 * Sets the host.
+	 *
+	 * @param host the new host
+	 */
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	/**
+	 * Gets the port.
+	 *
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * Sets the port.
+	 *
+	 * @param port the new port
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	/**
+	 * Gets the username.
+	 *
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * Sets the username.
+	 *
+	 * @param username the new username
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * Gets the password.
+	 *
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * Sets the password.
+	 *
+	 * @param password the new password
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
--- MavenBase/mail/src/main/java/com/example/mail/MailServiceImp.java
+++ MavenBase/mail/src/main/java/com/example/mail/MailServiceImp.java
@@ -1,5 +1,10 @@
 package com.example.mail;
 
+import java.util.Properties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,6 +12,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MailServiceImp implements MailService{
+	
+	/** The mail information. */
+	@Autowired
+	MailInformation mailInformation;
 
 	/* (non-Javadoc)
 	 * @see com.example.mail.MailService#SendMail()
@@ -14,7 +23,31 @@ public class MailServiceImp implements MailService{
 	@Override
 	public String SendMail() {
 
-		return "test";
+		try {
+			 // Create a mail sender
+	        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	        mailSender.setHost(mailInformation.getHost());
+	        mailSender.setPort(mailInformation.getPort());
+	        mailSender.setUsername(mailInformation.getUsername());
+	        mailSender.setPassword(mailInformation.getPassword());
+	        Properties props = new Properties();
+	        props.put("mail.smtp.starttls.enable", "true");
+//	        props.put("mail.smtp.ssl.enable", "true");
+	        mailSender.setJavaMailProperties(props);
+
+	        // Create an email instance
+	        SimpleMailMessage mailMessage = new SimpleMailMessage();
+	        mailMessage.setFrom("tuhuynh962285164@gmail.com");
+	        mailMessage.setTo("tuhuynh.work@outlook.com.vn");
+	        mailMessage.setText("test");
+
+//	         Send mail
+	        mailSender.send(mailMessage);
+			return "Successful";
+		}catch (Exception e) {
+			return "Failed: "+ e.getMessage();
+		}
+
 	}
 	
 	
